6e5f2cbeb038d638ecfae710090707930fb5e336
VictorPuska <vpuska@bigpond.net.au>
2018-05-13 17:16:14 +1000

    Test commit.

--- extensions/core/grid.py
+++ extensions/core/grid.py
@@ -7,7 +7,7 @@ Identification
     License:    LICENSE_NAME, see LICENSE_FILE for more details.
 
 Description
-    Authentication and authority management for the application.
+    Authentication and authority management for the application. Saving comment
 """
 
 import json
